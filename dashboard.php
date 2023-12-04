f4ca5f457ce0b1e1f9fcfc50a30e0212576f99a4
camilleMarques <143191420+camilleMarques@users.noreply.github.com>
2023-12-04 15:08:03 -0400

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if(empty($_SESSION)){
+    print "<script>location.href='index.php';</script>";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de login</title>
+</head>
+<body>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand">Instituto Federal 2023</a>
+            <?php 
+            print "Olá, ".$_SESSION["nome"];
+            print "<a bread='logout.php' class ='btn btn-danger'>Sair</a>";
+            ?>
+        </div>
+    </nav>
+</body>
+</html>
